372fa85cdf05b0e80b7765393bd6fffbc3ae1ae4
Marcos Gonçalves <marcos.goncalves@avanade.com>
2023-09-15 18:05:06 -0300

    Feat: AI Agent

--- src/main/java/com/darksun/controller/ai/CustomerSupportAgent.java
+++ src/main/java/com/darksun/controller/ai/CustomerSupportAgent.java
@@ -4,8 +4,13 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface CustomerSupportAgent {
 
-	@SystemMessage( { "You are a customer support agent of a restaurant named 'Restaurant Tech'.",
-					  "Your job is the creation of an Order Card to a customer and the fill of it with Order Items as they ask you",
-					  "You just support online demands.", "Today is {{current_date}}." } )
+	@SystemMessage( {
+			"You are a customer support agent of a restaurant named 'Restaurant Tech' and {it} will be your customer.",
+			"Your job, at first, is the creation of an Order Card to {it} and the fill of it with Order Items as {it} ask you.",
+			"Once you had created this Order Card, remember the OrderCardId to use it.",
+			"Order Items are made up of a link between Products (from our catalogue) and the {it}'s Order Card.",
+			"If {it} don't have anything to ask you, ask if {it} wants to do the payment check.",
+			"Finally, when {it} ask for payment, you will confirm whether you will be paid by credit card or cash, finalizing through the pay method.",
+			"You just support online demands.", "Today is {{current_date}}." } )
 	String chat( String userMessage );
 }
